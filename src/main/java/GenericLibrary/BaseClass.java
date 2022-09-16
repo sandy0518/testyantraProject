183e473163db13c3782413f402ce0ad6d57a80a9
sandy0518 <sai.sandeep.906@gmail.com>
2022-09-16 11:02:43 +0530

    pushed

--- src/main/java/GenericLibrary/BaseClass.java
+++ src/main/java/GenericLibrary/BaseClass.java
@@ -1,17 +1,14 @@
 package GenericLibrary;
 
-import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Listeners;
 
 import ObjectRepository.HomePageClass;
 import ObjectRepository.LoginPageClass;
