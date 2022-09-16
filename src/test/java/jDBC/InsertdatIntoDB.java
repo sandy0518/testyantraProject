952bb1c467bbf94e1c25b85f53c3343311b2e83b
sandy0518 <sai.sandeep.906@gmail.com>
2022-09-16 17:43:47 +0530

    two more programs

--- src/test/java/jDBC/InsertdatIntoDB.java
+++ src/test/java/jDBC/InsertdatIntoDB.java
@@ -5,13 +5,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 import org.testng.annotations.Test;
+
 import com.mysql.cj.jdbc.Driver;
 
-public class ConnectingToDBUsingTCBlock {
+public class InsertdatIntoDB {
 	@Test
-	public void readdataFromDataBase() throws SQLException {
-		
+	public void insertData() throws SQLException {
 		Connection con = null;
 		try {
 			//Step 1 :- Register the Driver
@@ -22,20 +23,23 @@ public class ConnectingToDBUsingTCBlock {
 			//step 3 :- Create Statement
 			Statement stmt = con.createStatement();
 			//Step 4 :- Execute query
-			ResultSet result = stmt.executeQuery("select * from students_info;");
-			while(result.next()) {
-				System.out.println(result.getString(1)+" "+result.getString(2)+" "+result.getString(3)+" "+result.getString(4));
+			int result = stmt.executeUpdate("insert into student_info (regno, firstrname, middlename, lastname) values(5,'nam','pallava','h');");
+			if(result==1) {
+				System.out.println("Data is Stored");
+			}
+			else {
+				System.out.println("Data is not stored");
 			}
 		} 
 		catch (SQLException e) {
 			// TODO Auto-generated catch block
-//			e.printStackTrace();
+			//		e.printStackTrace();
 			System.out.println("Exception Handled");
 		}
 		finally {
-		//Step 5 :- Close Connection with Database
-		con.close();
-		System.out.println("Connection closed");
+			//Step 5 :- Close Connection with Database
+			con.close();
+			System.out.println("Connection closed");
 		}
 	}
 }
