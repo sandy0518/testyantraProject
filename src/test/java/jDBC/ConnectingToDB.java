8519257c541c4978fca8f318e241468226f34b20
sandy0518 <sai.sandeep.906@gmail.com>
2022-09-22 10:57:03 +0530

    yes

--- src/test/java/jDBC/ConnectingToDB.java
+++ src/test/java/jDBC/ConnectingToDB.java
@@ -13,19 +13,19 @@ import com.mysql.cj.jdbc.Driver;
 public class ConnectingToDB {
 	@Test
 	public void readdataFromDataBase() throws SQLException {
-	//Step 1 :- Register the Driver
-	Driver dref = new Driver();
-	DriverManager.registerDriver(dref);
-	//Step 2 :- Connect to Database
-	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","root");
-	//step 3 :- Create Statement
-			Statement stmt = con.createStatement();
-	//Step 4 :- Execute query
-			ResultSet result = stmt.executeQuery("select * from student_info;");
-			while(result.next()) {
-				System.out.println(result.getString(1)+" "+result.getString(2)+" "+result.getString(3)+" "+result.getString(4));
-			}
-	//Step 5 :- Close Connection with Database
-			con.close();
+		//Step 1 :- Register the Driver
+		Driver dref = new Driver();
+		DriverManager.registerDriver(dref);
+		//Step 2 :- Connect to Database
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","root");
+		//step 3 :- Create Statement
+		Statement stmt = con.createStatement();
+		//Step 4 :- Execute query
+		ResultSet result = stmt.executeQuery("select * from student_info;");
+		while(result.next()) {
+			System.out.println(result.getString(1)+" "+result.getString(2)+" "+result.getString(3)+" "+result.getString(4));
+		}
+		//Step 5 :- Close Connection with Database
+		con.close();
 	}
 }
