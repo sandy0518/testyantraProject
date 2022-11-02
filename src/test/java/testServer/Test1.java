5942f3966d903df366748c6cfc04651096ae540f
sandy0518 <sai.sandeep.906@gmail.com>
2022-11-02 14:57:12 +0530

    jklmnb

---
+++ src/test/java/testServer/Test1.java
@@ -0,0 +1,15 @@
+package testServer;
+
+public class Test1 {
+	
+	static int a=20;
+	static int b=30;
+	
+	static class SubTest1{
+		public static void main(String[] args) {
+			System.out.println(a+b);
+		}
+	}
+	
+
+}
